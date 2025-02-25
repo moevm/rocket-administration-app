b53260ae079b319f5ddbc73ee439d46ee9c7ec55
raregodb <ggentelmann@yandex.ru>
2025-02-25 22:22:06 +0300

    feat: welcome page is done. closes #23

--- frontend/src/routes/welcome-page/WelcomePage.tsx
+++ frontend/src/routes/welcome-page/WelcomePage.tsx
@@ -1,28 +1,23 @@
 import {Label} from "@/components/ui/label.tsx";
 import {Button} from "@/components/ui/button.tsx";
-import {Card, CardContent, CardHeader} from "@/components/ui/card.tsx";
-import {Navigate, NavLink} from "react-router";
-import {User} from "lucide-react";
+import {NavLink} from "react-router";
 
 
 function WelcomePage () {
 
     return (
-        <div className={"flex items-center justify-center min-h-screen"}>
-            <Card>
-                <CardHeader className={"flex justify-center text-center"}>
-                    <Label>
-                        Добро пожаловать! <br/> Создайте свое первое пространство
-                    </Label>
-                </CardHeader>
-                <CardContent className={"flex justify-center"}>
-                    <Button>
-                        <NavLink to="/register-space">
-                            Клик!
-                        </NavLink>
-                    </Button>
-                </CardContent>
-            </Card>
+        <div className={"flex items-center justify-center min-h-screen flex-col gap-3 m-2"}>
+            <Label className={"text-2xl"}>
+                Добро пожаловать в <span className={"text-primary"}>RocketManager</span>
+            </Label>
+            <Label className={"text-foreground/60"}>
+                Приступим к настройке вашего первого пространства
+            </Label>
+            <Button>
+                <NavLink to="/register-space">
+                    Регистрация пространства
+                </NavLink>
+            </Button>
         </div>
 
     )
