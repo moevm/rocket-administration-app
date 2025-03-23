5c580b81d7b39f55b8bd5f0347694f13ea1e5f86
shT3rrr <03012005@inbox.ru>
2025-03-24 00:53:25 +0300

    feat: Был настроен возвращаемый тип, добавлен функционал отдачи всех полей.

--- backend/app/features/users/router.py
+++ backend/app/features/users/router.py
@@ -1,13 +1,12 @@
-from fastapi import APIRouter, Depends
-
+from fastapi import APIRouter, Depends, HTTPException, FastAPI
+from .models import UsersResponseDto
 from app.features.spaces.utils import get_space
 from app.lib.rocket import obtain_rocket_instance, key_for_space, rocket_request
-from app.services.db import get_db
 
 router = APIRouter()
 
 @router.get("/")
-async def get_users(db=Depends(get_db), space=Depends(get_space)):
+async def get_users(space=Depends(get_space)) -> UsersResponseDto:
     rocket = await obtain_rocket_instance(key_for_space(space))
-    users = await rocket_request(rocket.users_list)
+    users = UsersResponseDto.model_validate(await rocket_request(rocket.users_list))
     return users
