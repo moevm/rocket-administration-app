fa91bd035549cd28ee6d4e6fa5ddc831cf573255
raregodb <ggentelmann@yandex.ru>
2025-05-19 20:29:31 +0300

    feat: delete users from teams and rooms. refs #110

--- frontend/src/components/app/dialogs/user-page-dialogs/DeleteUserFromRoomDialog.tsx
+++ frontend/src/components/app/dialogs/user-page-dialogs/DeleteUserFromRoomDialog.tsx
@@ -1,6 +1,6 @@
 import {atom, useAtom, useAtomValue} from "jotai/index";
 import {BatchLoader} from "@/components/app/DataLoader.tsx";
-import {$rooms, $selectedUsersData} from "@/store/global-store.ts";
+import {$rooms, $selectedSpaceId, $selectedUsersData} from "@/store/global-store.ts";
 import {
     Dialog,
     DialogContent,
@@ -10,59 +10,93 @@ import {
     DialogTitle
 } from "@/components/ui/dialog.tsx";
 import {Button} from "@/components/ui/button.tsx";
+import {useEffect, useState} from "react";
+import {showAddUserInRoomDialogAtom} from "@/components/app/dialogs/user-page-dialogs/AddUserInRoomDialog.tsx";
+import RoomSmallTableView from "@/components/app/table/RoomSmallTableView.tsx";
+import {$api, createMutationOptions, loaded} from "@/api";
+import ExportCard from "@/components/app/dialogs/ExportCard.tsx";
 
 export const showDeleteUserFromRoomDialogAtom = atom(false)
 
-function DeleteUserFromRoomContent() {
+function DeleteUserFromRoomContent(props: {
+    smallRooms: { _id: string, name: string | null | undefined }[]
+}) {
     const [open, setOpen] = useAtom(showDeleteUserFromRoomDialogAtom)
-    // const selectedSpaceId = useAtomValue($selectedSpaceId)!
+    const [dialogStep, setDialogStep] = useState(1);
+    const selectedSpaceId = useAtomValue($selectedSpaceId)!
     const selectedUsersData = useAtomValue($selectedUsersData)
-    // const [selectedRoomIds, setSelectedRoomIds] = useState<string[]>([]);
+    const [selectedRoomIds, setSelectedRoomIds] = useState<string[]>([]);
+    const [results, setResults] = useState<object[]>([]);
 
-    // const {
-    //     mutate,
-    //     isPending
-    // } = $api.useMutation('post', '/spaces/{space_id}/user_room/', createMutationOptions({
-    //     onSuccess: async (data) => {
-    //         console.log(data)
-    //     }
-    // }))
 
-    const handleAddClick = () => {
-        // mutate({
-        //     body: {
-        //         users: selectedUsersData.map(it => it.username),
-        //         rooms: selectedRoomIds
-        //     },
-        //     params: {
-        //         path: {
-        //             space_id: selectedSpaceId!
-        //         },
-        //     }
-        // })
+    useEffect(() => {
+        if (!open) {
+            setDialogStep(1)
+            setResults([])
+        }
+    }, [open]);
+
+    const {
+        mutate,
+        isPending
+    } = $api.useMutation('post', '/spaces/{space_id}/user_room/remove', createMutationOptions({
+        onSuccess: async (data) => {
+            setDialogStep(0)
+            setResults(data)
+            console.log(data)
+        }
+    }))
+
+    const handleSubmit = () => {
+        mutate({
+            body: {
+                users: selectedUsersData.map(it => it._id),
+                rooms: selectedRoomIds
+            },
+            params: {
+                path: {
+                    space_id: selectedSpaceId!
+                },
+            }
+        })
     };
 
     return (
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogContent className="max-w-2xl">
-                <DialogHeader>
-                    <DialogTitle>Удалить из комнаты</DialogTitle>
-                    <DialogDescription>
-                        Выбрано пользователей: {selectedUsersData.length}
-                    </DialogDescription>
-                </DialogHeader>
+                {dialogStep === 1 ? (
+                    <>
+                        <DialogHeader>
+                            <DialogTitle>Удалить из комнаты</DialogTitle>
+                            <DialogDescription>
+                                Выбрано пользователей: {selectedUsersData.length}
+                            </DialogDescription>
+                        </DialogHeader>
 
-                {/*<div className="max-h-[60vh] overflow-y-auto">*/}
-                {/*    <RoomSmallTableView data={props.smallRooms} onSelectionUpdated={data =>*/}
-                {/*        setSelectedRoomIds(data.map(it => it.getValue('_id')))*/}
-                {/*    }/>*/}
-                {/*</div>*/}
+                        <div className="max-h-[60vh] overflow-y-auto">
+                            <RoomSmallTableView data={props.smallRooms} onSelectionUpdated={data =>
+                                setSelectedRoomIds(data.map(it => it.getValue('_id')))
+                            }/>
+                        </div>
 
-                <DialogFooter className="sm:justify-start">
-                    <Button type="button" variant="default">
-                        Удалить
-                    </Button>
-                </DialogFooter>
+                        <DialogFooter className="sm:justify-start">
+                            <Button type="button" variant="default" onClick={handleSubmit}>
+                                Удалить
+                            </Button>
+                        </DialogFooter>
+                    </>
+                ) : (
+                    <>
+                        <DialogHeader>
+                            <DialogTitle>Удаление пользователя из комнаты</DialogTitle>
+                        </DialogHeader>
+                        <ExportCard data={results} showData={true} countedValues={[
+                            {key: 'success', display: 'Успешно удалено'},
+                            {key: 'error', display: 'Ошибок'},
+                        ]} />
+                    </>
+                )}
+
             </DialogContent>
         </Dialog>
     )
@@ -70,13 +104,17 @@ function DeleteUserFromRoomContent() {
 
 function DeleteUserFromRoomDialog() {
     const rooms = useAtomValue($rooms)
+    const selectedUsersData = useAtomValue($selectedUsersData)
+
 
     return (
-        <BatchLoader
-            states={[rooms]}
-            loadingMessage='Загрузка комнат'
-            display={() => <DeleteUserFromRoomContent/>}
-        />
+        <>
+            <BatchLoader
+                states={[rooms]}
+                loadingMessage='Загрузка комнат'
+                display={() => <DeleteUserFromRoomContent smallRooms={loaded(rooms).data.map(({_id, name}) => ({_id, name}))}/>}
+            />
+        </>
     )
 }
 
