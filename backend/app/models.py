652963009c9d0901e930fe837a3a90fe8ea930ec
shT3rrr <03012005@inbox.ru>
2025-05-20 00:11:49 +0300

    feat: Были добавлены 2 эндпоинта (для добавления и удаления комнат в(из) команду(-ы))

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,10 +4,53 @@ from app.services.db import DbModel
 from datetime import datetime
 
 
+class TeamsAndRoomsDto(BaseModel):
+    teams: List[str]
+    rooms: List[str]
+
+
+class TeamsAndRoomsResDto(BaseModel):
+    success: bool = True
+    error: Optional[str] = None
+    room: str
+    team: str
+
+
+class ShortUserDto(BaseModel):
+    id: str = Field(alias='_id')
+    username: str
+
+
+class RoomDto(BaseModel):
+    id: str = Field(alias='_id')
+    description: Optional[str] = None
+    broadcast: Optional[bool] = None
+    name: Optional[str] = None
+    t: str = None
+    msgs: Optional[int] = None
+    usersCount: Optional[int] = None
+    u: Optional[ShortUserDto] = None
+    ro: Optional[bool] = None
+    default: Optional[bool] = None
+    topic: Optional[str] = None
+    announcement: Optional[str] = None
+
+
+class TeamDto(BaseModel):
+    id: str = Field(alias='_id')
+    name: Optional[str] = None
+    type: Optional[int] = None
+    createdAt: Optional[str] = None
+    createdBy: Optional[ShortUserDto] = None
+    updatedAt: Optional[str] = None
+    roomId: Optional[str] = None
+
+
 class UserCreateDto(BaseModel):
     username: str
     email: EmailStr
     name: str
+
 
 class UsersImportRequestDto(BaseModel):
     users: List[UserCreateDto]
@@ -58,11 +101,6 @@ class ImportedTeamResultDto(BaseModel):
     error: Optional[str] = None
 
 
-class ShortUserDto(BaseModel):
-    id: str = Field(alias='_id')
-    username: str
-
-
 class UserEmailDto(BaseModel):
     address: str
     verified: bool
@@ -98,26 +136,13 @@ class RoleDto(BaseModel):
     protected: bool
 
 
-class RoomDto(BaseModel):
-    id: str = Field(alias='_id')
-    description: Optional[str] = None
-    broadcast: Optional[bool] = None
-    name: Optional[str] = None
-    t: str = None
-    msgs: Optional[int] = None
-    usersCount: Optional[int] = None
-    u: Optional[ShortUserDto] = None
-    ro: Optional[bool] = None
-    default: Optional[bool] = None
-    topic: Optional[str] = None
-    announcement: Optional[str] = None
-
 class UserInfoRoomDto(BaseModel):
     id: str = Field(alias='_id')
     name: str
     rid: str
     t: str
     roles: Optional[List[str]] = None
+
 
 class RoomUserDto(BaseModel):
     id: str = Field(alias='_id')
@@ -149,41 +174,39 @@ class SpaceDto(SpaceModel):
     pass
 
 
-class TeamDto(BaseModel):
-    id: str = Field(alias='_id')
-    name: Optional[str] = None
-    type: Optional[int] = None
-    createdAt: Optional[str] = None
-    createdBy: Optional[ShortUserDto] = None
-    updatedAt: Optional[str] = None
-    roomId: Optional[str] = None
-
 class TeamInfoDto(BaseModel):
     users: List[RoomUserDto]
     rooms: List[RoomDto]
+
 
 class UserInfoDto(BaseModel):
     teams: List[ShortTeamDto]
     rooms: List[UserInfoRoomDto]
 
+
 class SmtpSettingsDto(BaseModel):
     host: AnyUrl
     sender: EmailStr
+
 
 class SmtpSettingsModel(DbModel):
     host: AnyUrl
     sender: EmailStr
 
+
 class SmtpSettingsResponseDto(BaseModel):
     value: Optional[SmtpSettingsDto]
+
 
 class UsersToChangePasswordDto(BaseModel):
     users: List[str]
     sendEmail: bool
 
+
 class Result[T](BaseModel):
     value: Optional[T] = None
     error: Optional[str] = None
+
 
 class ChangedPasswordDto(BaseModel):
     user: str
@@ -192,9 +215,11 @@ class ChangedPasswordDto(BaseModel):
     email_sent: bool = False
     email_send_error: Optional[str] = None
 
+
 class UsersAndRoomsDto(BaseModel):
     users: List[str]
     rooms: List[str]
+
 
 class UsersAndRoomsResDto(BaseModel):
     success: bool = True
