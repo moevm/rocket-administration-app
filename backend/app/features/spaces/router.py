01c5a76bfd3c8ea451da8d95411c97584252e0b2
shT3rrr <03012005@inbox.ru>
2025-05-20 23:16:42 +0300

    feat: Был добавлен эндпоинт проверки валидности токена в текущем пространстве

--- backend/app/features/spaces/router.py
+++ backend/app/features/spaces/router.py
@@ -6,6 +6,8 @@ from app.lib.cache import key_for_space
 from app.models import CreateSpaceRequest, SpaceDto, SpaceModel
 from app.lib.rocket import obtain_rocket_instance
 from app.services.db import get_db, convert_to
+from app.features.spaces.utils import get_space
+from app.lib.rocket import obtain_rocket_instance, rocket_request, rocket_query_args
 
 router = APIRouter()
 
@@ -18,10 +20,27 @@ async def create_space(create_space_request: CreateSpaceRequest, db=Depends(get_
     space_model = SpaceModel.model_validate(await db.spaces.find_one({"_id": insert_result.inserted_id}))
     return convert_to(SpaceDto, space_model)
 
-
 @router.get("/")
 async def get_spaces(db=Depends(get_db)) -> List[SpaceDto]:
     result = []
     async for doc in db.spaces.find():
         result.append(convert_to(SpaceDto, SpaceModel.model_validate(doc)))
     return result
+
+@router.get("/{space_id}/status")
+async def get_user_status(space=Depends(get_space)):
+
+    try:
+        rocket = await obtain_rocket_instance(key_for_space(space))
+
+        await rocket_request(
+            rocket.me,
+            **rocket_query_args()
+        )
+        return {"status": "ok"}
+
+    except Exception as e:
+        if "You must be logged in to do this." in str(e):
+            return {"status": "unauthorized"}
+        else:
+            return {"status": "unknown-error"}
