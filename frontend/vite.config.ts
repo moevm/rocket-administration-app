c1d244acbd148e55eb761843e38fa2e962594106
Sepesch <sp1.8@mail.ru>
2026-05-24 20:19:03 +0300

    вернул фронт в прежнее состояние, потому что не требовал изменений

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,14 +9,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-    proxy: {
-      "/spaces": {
-        target: "http://backend:8000",
-        changeOrigin: true
-      }
-    }
-  }
 })
